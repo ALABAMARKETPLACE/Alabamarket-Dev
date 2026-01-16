57cd3bd82e726488c3ad2bc0bcb62313ab61d449
Your Name <you@example.com>
2026-01-16 08:34:44 +0100

    Done

--- src/app/(dashboard)/auth/products/page.tsx
+++ src/app/(dashboard)/auth/products/page.tsx
@@ -132,10 +132,7 @@ function Products() {
       <div className="products-pageHeaderWrap">
         <PageHeader title={"Products"} bredcume={"Dashboard / Products"}>
           <div className="products-headerActions">
-            <div
-              className="products-headerPrimary"
-              style={{ marginRight: "20px" }}
-            >
+            <div className="products-headerPrimary">
               {" "}
               <Button
                 type="primary"
