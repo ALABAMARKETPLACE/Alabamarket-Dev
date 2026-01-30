c31a6de7fc72abff479a719c5880d1e0767db1eb
Your Name <you@example.com>
2026-01-30 15:43:20 +0100

    Done

--- src/components/chatbot/RuleBasedChatBot.tsx
+++ src/components/chatbot/RuleBasedChatBot.tsx
@@ -147,7 +147,9 @@ const RuleBasedChatBot: React.FC = () => {
                 key={message.id}
                 className={`chatbot-message-wrapper chatbot-message-wrapper--${message.sender}`}
               >
-                <div className={`chatbot-message chatbot-message--${message.sender}`}>
+                <div
+                  className={`chatbot-message chatbot-message--${message.sender}`}
+                >
                   <div className="chatbot-message-bubble">
                     <p className="chatbot-message-text">{message.text}</p>
                   </div>
